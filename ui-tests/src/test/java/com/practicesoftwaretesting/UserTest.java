6ed473dbd38b8afd83334fd7f6acbb573e8cf3aa
anastasiya-harbatsevich <nastya7535@gmail.com>
2024-07-18 13:46:26 +0200

    Fixed query phrase and creating constant

--- ui-tests/src/test/java/com/practicesoftwaretesting/UserTest.java
+++ ui-tests/src/test/java/com/practicesoftwaretesting/UserTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 public class UserTest extends BaseTest {
 
+    private static final String LAST_NAME = "Lindeman";
+
     HomePage homePage = new HomePage();
     Header header = new Header();
     LoginPage loginPage = new LoginPage();
@@ -36,14 +38,14 @@ public class UserTest extends BaseTest {
         accountPage.isLoaded();
         header.assertThat().isSignedIn(user.getFirstName() + " " + user.getLastName());
 
-        var users = searchUsers("Bristol");
+        var users = searchUsers(LAST_NAME);
         users.getData().forEach(userToDelete -> deleteUser(userToDelete.getId()));
     }
 
     private RegisterUserRequest getUser() {
         return RegisterUserRequest.builder()
                 .firstName("Denis")
-                .lastName("Lindeman")
+                .lastName(LAST_NAME)
                 .dob("12.12.2000")
                 .address("12 aleja Jerozolimskich")
                 .city("Bristol")
